971db15b3549110f44b706a49f0e3a27fc0c81f7
MansourS02 <elhadjimansoursow9@gmail.COM>
2026-09-24 15:56:16 +0000

    Fusion avec gestfin

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-<<<<<<< HEAD
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-=======
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
->>>>>>> origin/main
   server: {
     host: "::",
     port: 8080,
@@ -18,11 +11,7 @@ export default defineConfig(() => ({
       overlay: false,
     },
   },
-<<<<<<< HEAD
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-=======
   plugins: [react()],
->>>>>>> origin/main
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
